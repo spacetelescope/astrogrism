bffdf4585ddbd07582d7a4759e2b8f21ab7e9f45
Duy Tuong Nguyen <dtn5ah@virginia.edu>
2021-08-25 10:26:42 -0400

    Remove grizli test

--- astrogrism/tests/test_wfc3_g102.py
+++ astrogrism/tests/test_wfc3_g102.py
@@ -14,7 +14,7 @@ test_dir = pathlib.Path(__file__).parent.absolute()
 grism_image_file = 'https://github.com/npirzkal/aXe_WFC3_Cookbook/raw/main/cookbook_data/G141/ib6o23rsq_flt.fits' # noqa
 
 
-def test_wfc3_g141_astropywcs():
+def test_wfc3_g102_astropywcs():
     """
     Tests the Astrogrism Detector > World Transform against
     the built in Astropy WCS model
@@ -88,29 +88,6 @@ def test_wfc3_g102_grismconf():
     # Compare results
     np.testing.assert_allclose(astrogrism_x, grismconf_x, atol=5e-02)
     np.testing.assert_allclose(astrogrism_y, grismconf_y, atol=5e-02)
-
-
-def test_wfc3_g102_grizli():
-    """
-    Tests the Astrogrism Detector > Grism Transform against
-    grizli's grism transform
-    """
-    # TODO: Calculate Grizli values dynamically rather than rely on stored vals
-
-    # Calculate Astrogrism dispersed Xs and Ys for given wavelengths
-    grizli_wavelengths_file = pathlib.Path(test_dir,
-                                           'grizli_wfc3_g102_solutions',
-                                           'grizli_wavelength_solutions.npy')
-    grizli_wavelengths = np.load(grizli_wavelengths_file)
-    astrogrism_x, astrogrism_y = _image2grism(500, 500, grizli_wavelengths)
-
-    # Compare results
-    np.testing.assert_allclose(astrogrism_x, 500 + np.arange(500), atol=5e-02)
-    grizli_y_file = pathlib.Path(test_dir,
-                                 'grizli_wfc3_g102_solutions',
-                                 'grizli_y_solutions.npy')
-    grizli_y = np.load(grizli_y_file)
-    np.testing.assert_allclose(astrogrism_y, grizli_y, atol=5e-02)
 
 
 def _image2grism(x_center, y_center, wavelengths):
