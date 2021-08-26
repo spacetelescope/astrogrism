e582319380a8fb08798fc966136f82a9e4a8b56e
Duy Nguyen <dtn5ah@virginia.edu>
2021-08-26 18:02:22 -0400

    Grizli test use generic image2grism tes generator

--- astrogrism/tests/test_wfc3_g141_grizli.py
+++ astrogrism/tests/test_wfc3_g141_grizli.py
@@ -1,5 +1,4 @@
-from astrogrism import GrismObs
-import grismconf
+from astrogrism.tests.test_image2grism import _image2grism
 
 import pathlib
 from astropy.utils.data import download_file
@@ -34,21 +33,3 @@ def test_wfc3_g141_grizli():
                                  'grizli_y_solutions.npy')
     grizli_y = np.load(grizli_y_file)
     np.testing.assert_allclose(astrogrism_y, grizli_y, atol=5e-02)
-
-
-def _image2grism(x_center, y_center, wavelengths, grism_file=None):
-    if not grism_file:
-        raise NotImplementedError("Grism FLT File is required for now")
-    fn = download_file(grism_file, cache=True)
-    grismobs = GrismObs(fn)
-    image2grism = grismobs.geometric_transforms.get_transform('detector',
-                                                              'grism_detector')
-    x, y = list(), list()
-    for val in wavelengths:
-        dispersion = image2grism.evaluate(x_center,
-                                          y_center,
-                                          wavelength=val,
-                                          order=1)
-        x.append(dispersion[0])
-        y.append(dispersion[1])
-    return x, y
