4f9da5236684081c58e6cf543fed7c528f917f2a
Duy Nguyen <dtn5ah@virginia.edu>
2021-08-26 18:00:04 -0400

    Parametrize image2world tests

--- astrogrism/tests/test_wfc3_g141.py
+++ astrogrism/tests/test_wfc3_g141.py
@@ -2,48 +2,12 @@ from astrogrism import GrismObs
 import grismconf
 
 import pathlib
-import tempfile
-from zipfile import ZipFile
 from astropy.utils.data import download_file
-from astropy.io import fits
-from astropy.wcs import WCS
 import numpy as np
 
 # TODO: Switch to importlib
 test_dir = pathlib.Path(__file__).parent.absolute()
 G141_IMAGE_FILE = 'https://github.com/npirzkal/aXe_WFC3_Cookbook/raw/main/cookbook_data/G141/ib6o23rsq_flt.fits' # noqa
-
-
-def test_wfc3_g141_astropywcs():
-    """
-    Tests the Astrogrism Detector > World Transform against
-    the built in Astropy WCS model
-    """
-    # Define Pixel Grid
-    xx = np.arange(0, 1014, 20)
-    yy = np.arange(0, 1014, 20)
-    # Download file and initialize wcs
-    fn = download_file(G141_IMAGE_FILE, cache=True)
-    grism_image_hdulist = fits.open(fn)
-    astropy_wcs = WCS(grism_image_hdulist['SCI'].header)
-    # Convert Pixel Grid to Sky Grid
-    astropy_coords = astropy_wcs.pixel_to_world(xx, yy)
-    # Extract sky coordinates
-    astropywcs_ra = astropy_coords.ra.value
-    astropywcs_dec = astropy_coords.dec.value
-
-    # Init GrismObs
-    fn = download_file(G141_IMAGE_FILE, cache=True)
-    grismobs = GrismObs(fn)
-    # Retrieve Transform
-    image2world = grismobs.geometric_transforms.get_transform('detector',
-                                                              'world')
-    # Calculate sky coordinates on grid
-    astrogrism_ra, astrogrism_dec = image2world(xx, yy, 0, 0)[:2]
-
-    # Compare results
-    np.testing.assert_allclose(astrogrism_ra, astropywcs_ra, atol=5e-02)
-    np.testing.assert_allclose(astrogrism_dec, astropywcs_dec, atol=5e-02)
 
 
 def test_wfc3_g141_grizli():
