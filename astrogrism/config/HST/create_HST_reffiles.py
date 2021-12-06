a9c595f22e311a5deacaecf4f57098e81764cee9
duytnguyendtn <duytnguyendtn.open@gmail.com>
2021-12-06 10:31:18 -0500

    Codestyle

--- astrogrism/config/HST/create_HST_reffiles.py
+++ astrogrism/config/HST/create_HST_reffiles.py
@@ -8,12 +8,10 @@ from astrogrism.config.HST.reference_file_generators._generate_wavelengthrange i
 def create_reference_files(conffile, hst_grism, outpath=Path.cwd(), outbasename=None):
     if outbasename is None:
         outbasename = Path(conffile).name
-    create_tsgrism_wavelengthrange(outname=str(Path(outpath) / (
-                                                                str(outbasename) +
-                                                                "_wavelengthrange.asdf")
-                                               )
-                                   )
-    create_grism_specwcs(conffile=str(conffile),
-                         pupil=hst_grism,
-                         outname=str(Path(outpath) / (str(outbasename) + "_specwcs.asdf")))
+
+    wavelengthrange_filename = str(Path(outpath) / (str(outbasename) + "_wavelengthrange.asdf"))
+    create_tsgrism_wavelengthrange(outname=str(wavelengthrange_filename))
+
+    specwcs_filename = str(Path(outpath) / (str(outbasename) + "_specwcs.asdf"))
+    create_grism_specwcs(conffile=str(conffile), pupil=hst_grism, outname=str(specwcs_filename))
     # create_distortion(detector, apname, outname, subarr, exp_type)
