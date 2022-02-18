dd48f54fa942e8672ab4b9e90390a4c141e1e25a
Ricky O'Steen <rosteen@stsci.edu>
2022-02-18 10:44:20 -0500

    Add UVIS subarray tests

---
+++ astrogrism/tests/test_wfc3_subarrays.py
@@ -0,0 +1,59 @@
+from astrogrism import GrismObs
+
+import pathlib
+import numpy as np
+import astropy.units as u
+from astropy.tests.helper import assert_quantity_allclose
+
+# TODO: Switch to importlib
+test_dir = pathlib.Path(__file__).parent.absolute()
+
+
+def test_uvis_subarray():
+    """
+    Tests Astrogrism transforms round tripping as expected between grism
+    detector, direct detector and world frames.
+    """
+    test_file = pathlib.Path(test_dir, "data", "uvis_subarray_test_file.fits")
+    grism_obs = GrismObs(str(test_file))
+    grism_hdr = grism_obs.grism_image[1].header
+
+    assert grism_obs.geometric_transforms["CCD2"].available_frames == ['grism_detector',
+                                                                       'detector', 'world']
+
+    # Check that detector -> grism is working before checking full transform
+    d2g2 = grism_obs.geometric_transforms["CCD2"].get_transform("detector", "grism_detector")
+
+    d2g2_expected = (418.3477, 1673.770, 600.0, 1500.0, 1.0)
+
+    np.testing.assert_allclose(d2g2(600.0, 1500.0, 4000, 1.0), d2g2_expected, atol=5e-2)
+
+    # Now test transforming all the way end to end
+    world_ref2 = (grism_hdr["CRVAL1"], grism_hdr["CRVAL2"], 4000*u.AA, 1.0)
+
+    g2w2 = grism_obs.geometric_transforms["CCD2"].get_transform("grism_detector", "world")
+    w2g2 = grism_obs.geometric_transforms["CCD2"].get_transform("world", "grism_detector")
+
+    for key, value in grism_hdr.items():
+        print(f"{key}: {value}")
+    w2g2_expected = (1.874515e+03, -3.091660e+02, grism_hdr["CRPIX1"]-1, grism_hdr["CRPIX2"]-1, 1.0)
+
+    np.testing.assert_allclose(w2g2(*world_ref2), w2g2_expected, rtol=5e-6)
+
+    # Use rtol here because the wavelength doesn't round trip perfectly
+    g2w2_res = g2w2(*w2g2_expected)
+    [assert_quantity_allclose(g2w2_res[i], world_ref2[i], rtol=0.005) for i in
+     range(len(world_ref2))]
+
+    # Test world <-> detector transforms
+    w2d_expected = (grism_hdr["CRPIX1"]-1, grism_hdr["CRPIX2"]-1, 4000*u.AA, 1.0)
+
+    d2w2 = grism_obs.geometric_transforms["CCD2"].get_transform("detector", "world")
+    w2d2 = grism_obs.geometric_transforms["CCD2"].get_transform("world", "detector")
+
+    [assert_quantity_allclose(w2d2(*world_ref2)[i], w2d_expected[i], rtol=5e-5) for
+     i in range(len(world_ref2))]
+
+    d2w2_res = d2w2(*w2d_expected)
+    [assert_quantity_allclose(d2w2_res[i], world_ref2[i], rtol=0.005) for i in
+     range(len(world_ref2))]
