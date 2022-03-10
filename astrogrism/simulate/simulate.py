4b28ae734593026224b2c9f8e71d3dc7d32fa44d
duytnguyendtn <duytnguyendtn.open@gmail.com>
2022-03-10 15:21:23 -0500

    Codestyle

--- astrogrism/simulate/simulate.py
+++ astrogrism/simulate/simulate.py
@@ -28,7 +28,7 @@ def _download_stsynphot_files(download_path):
     if not vega_reference_atlas_path.exists():
         vega_reference_atlas_path.parent.mkdir(parents=True, exist_ok=True)
         archive_url = ('https://archive.stsci.edu/hlsps/reference-atlases/cdbs/'
-                        'current_calspec/alpha_lyr_stis_010.fits')
+                       'current_calspec/alpha_lyr_stis_010.fits')
         temp_download = Path(download_file(archive_url, cache=True))
         copy(str(temp_download), str(vega_reference_atlas_path))
 
