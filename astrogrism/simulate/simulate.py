520d5835d996f00bfac218521bc3c12bc73840ae
Duy Tuong Nguyen <dtn5ah@virginia.edu>
2022-03-10 14:50:59 -0500

    Codestyle
    Co-authored-by: Ricky O'Steen <39831871+rosteen@users.noreply.github.com>

--- astrogrism/simulate/simulate.py
+++ astrogrism/simulate/simulate.py
@@ -49,7 +49,9 @@ def generate_simulation_spectrum(grism, detector=None, temp_path=gettempdir()):
     # Check if it exists first before trying to download it
     if not vega_reference_atlas_path.exists():
         vega_reference_atlas_path.parent.mkdir(parents=True, exist_ok=True)
-        temp_download = Path(download_file('https://archive.stsci.edu/hlsps/reference-atlases/cdbs/current_calspec/alpha_lyr_stis_010.fits', cache=True)) # noqa
+        archive_url = ('https://archive.stsci.edu/hlsps/reference-atlases/cdbs/'
+                       'current_calspec/alpha_lyr_stis_010.fits')
+        temp_download = Path(download_file(archive_url, cache=True))
         copy(str(temp_download), str(vega_reference_atlas_path))
 
     # Now that we have all our reference files, we can import stsynphot
