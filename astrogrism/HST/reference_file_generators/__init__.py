679bfca07168923e928fa796cd1402a4661a6c93
Ricky O'Steen <rosteen@stsci.edu>
2022-03-17 14:13:25 -0400

    Update package data to include new folder, correct __init__ import

--- astrogrism/HST/reference_file_generators/__init__.py
+++ astrogrism/HST/reference_file_generators/__init__.py
@@ -1,2 +1,2 @@
 from .generate_specwcs import create_grism_specwcs  # noqa: F401
-from .generate_wavelengthrange import create_tsgrism_wavelengthrange  # noqa: F401
+from .generate_wavelengthrange import create_grism_wavelengthrange  # noqa: F401
